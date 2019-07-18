1399f159dac5b0a4b25131d22d5ee0b74402030f
Rahul Shukla <shukla2009@gmail.com>
2019-07-18 22:27:54 +0800

    feat: add load test capabilities

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 'use strict';
-
+process.title = 'RideApp';
 const port = 8010;
 const Sequelize = require('sequelize');
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database(':memory:');
 const logger = require('./logger')(module)
 const sequelize = new Sequelize({
-    dialect: 'sqlite'
+    dialect: 'sqlite',
+    logging: false
   });
 
 db.serialize(() => {
-    //buildSchemas(db);
     const app = require('./src/app')(sequelize);
     sequelize.sync();
     app.listen(port, () => logger.info(`App started and listening on port ${port}`));
